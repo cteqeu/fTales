cfcadc3bb3173e5a3abe1a8b68b356bdf3588f94
Hogeschool PXL - Electronics-ICT <vincent.claes@pxl.be>
2017-04-24 10:09:42 +0200

    Update
    Add Directories
    Add HelloWorld

---
+++ HelloWorld/HelloWorld/HelloWorld.ino
@@ -0,0 +1,27 @@
+// Hello World by Vincent Claes - Hogeschool PXL //
+
+
+int pushButton = PUSH2;
+int greenPin= GREEN_LED;
+int redPin= RED_LED;
+int yellowPin= YELLOW_LED;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pushButton, INPUT_PULLUP);
+  pinMode(greenPin,OUTPUT);
+  pinMode(redPin,OUTPUT);
+  pinMode(yellowPin,OUTPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(pushButton);
+  Serial.println(buttonState);
+  digitalWrite(greenPin, buttonState);
+  digitalWrite(redPin, buttonState);
+  digitalWrite(yellowPin, buttonState);
+  delay(500);
+}
+
+
+
